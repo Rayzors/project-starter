621b302908d789d815c50fb9b98d0b7c9eb87185
Kevin <kevin.manssat@hotmail.fr>
2020-06-24 19:54:17 +0200

    add server && client files

---
+++ client/nuxt.config.js
@@ -0,0 +1,68 @@
+console.log(process.env.API_URL)
+export default {
+  server: {
+    host: '0.0.0.0'
+  },
+  env: {
+    API_URL: process.env.API_URL
+  },
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    // Doc: https://github.com/nuxt-community/stylelint-module
+    '@nuxtjs/stylelint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ['@nuxtjs/pwa', '@nuxtjs/apollo'],
+  apollo: {
+    errorHandler: '~/plugins/apollo/error-handler.js',
+    clientConfigs: {
+      default: {
+        httpEndpoint: process.env.API_URL || "http://localhost:1337/graphql"
+      }
+    }
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+}
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2c4dc0b3-ae8f-4cf2-8e79-e31671ff1a2e'
+};
